efd2e781711d0b3c8cbd281ebe5e3280e6095c56
Adam Wenocur <wenocur@email.chop.edu>
2014-08-09 03:01:29 -0400

    interim fix for genotype_value

--- vdw/samples/models.py
+++ vdw/samples/models.py
@@ -365,13 +365,18 @@ class Result(TimestampedModel):
     @property
     def genotype_value(self):
         if self.genotype:
-            variant = self.variant
-            if genotype in ('1/1', '1/2'):
-                return variant.alt + '/' + variant.alt
+            genotype = self.genotype.value
+            allele_1 = self.allele_1
+            allele_2 = self.allele_2
+            if genotype == '1/1':
+                return allele_1.alt + '/' + allele_1.alt
+            elif genotype == '1/2':
+                return allele_1.alt + '/' + (allele_2.alt if allele_2 else
+                                             allele_1.alt)
             elif genotype == '0/1':
-                return variant.ref + '/' + variant.alt
+                return allele_1.ref + '/' + allele_1.alt
             elif genotype == '0/0':
-                return variant.ref + '/' + variant.alt
+                return allele_1.ref + '/' + allele_1.ref
 
     @property
     def base_count_map(self):
